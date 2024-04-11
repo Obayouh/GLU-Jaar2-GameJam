2a4997397343451162219073ecabe212600da1da
Casper <casper.ketelaars@icloud.com>
2024-04-11 11:57:59 +0200

    Cards spawn in hand

---
+++ GameJam_Itch_io/Assets/Scripts/Card-related stuff/CardManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public GameObject[] cards;
+    public List<GameObject> spawnedCards = new List<GameObject>();
+    public Transform[] cardPositions;
+
+    int maxAmountOfCards = 4;
+
+    void Start()
+    {
+        for (int i = 0; i < maxAmountOfCards; i++)
+        {
+            int rdm = Random.Range(0, cards.Length);
+            GameObject card = Instantiate(cards[rdm], cardPositions[i]);
+            spawnedCards.Add(card);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
