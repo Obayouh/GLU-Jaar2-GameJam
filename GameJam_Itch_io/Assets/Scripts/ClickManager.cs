a647fd1c1d3646abe4c3fd5991c15504efc1feec
Casper <casper.ketelaars@icloud.com>
2024-04-12 10:11:11 +0200

    Veel Shit

--- GameJam_Itch_io/Assets/Scripts/ClickManager.cs
+++ GameJam_Itch_io/Assets/Scripts/ClickManager.cs
@@ -16,31 +16,36 @@ public class ClickManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 100.0f))
-            {
-                currentHitTransform = hit.transform;
-                hitPosition = currentHitTransform.position;
+            currentHitTransform = hit.transform;
+            hitPosition = currentHitTransform.position;
 
-                if(currentCoroutine == null)
-                {
-                    currentCoroutine = StartCoroutine(ScaleMe(hit.transform));
-                }
-                //Debug.Log("You selected the " + hit.transform.name);
-                //Debug.Log(currentHitTransform.tag);
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+
+            if (currentCoroutine == null)
+            {
+                currentCoroutine = StartCoroutine(ScaleMe(hit.transform));
+                Debug.Log("You selected the " + hit.transform.name);
             }
         }
 
     }
     IEnumerator ScaleMe(Transform objTr)
     {
-        if (objTr.CompareTag("Enemy") || objTr.CompareTag("PlayerCard"))
+        if (objTr.CompareTag("Enemy"))
         {
             objTr.localScale *= 1.2f;
             yield return new WaitForSeconds(0.5f);
             objTr.localScale /= 1.2f;
+        }
+        else if (objTr.CompareTag("PlayerCard"))
+        {
+
         }
         currentCoroutine = null;
     }
