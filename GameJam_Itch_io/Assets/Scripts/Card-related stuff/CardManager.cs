cd0b4fe721803fc0339d29d1c4e37aa5c970b5d4
Casper <casper.ketelaars@icloud.com>
2024-04-12 12:28:04 +0200

    Remove card en AddCards activeer dat in de spelers nieuwe turn

--- GameJam_Itch_io/Assets/Scripts/Card-related stuff/CardManager.cs
+++ GameJam_Itch_io/Assets/Scripts/Card-related stuff/CardManager.cs
@@ -19,4 +19,25 @@ public class CardManager : MonoBehaviour
             spawnedCards.Add(card);
         }
     }
+
+    public void RemoveCard(GameObject card)
+    {
+        spawnedCards.Remove(card);
+    }
+
+    public void AddCards()
+    {
+        if (spawnedCards.Count < maxAmountOfCards)
+        {
+            for (int i = 0; i < maxAmountOfCards; i++)
+            {
+                if (cardPositions[i].childCount >= 0)
+                {
+                    int rdm = Random.Range(0, cards.Length);
+                    GameObject card = Instantiate(cards[rdm], cardPositions[i]);
+                    spawnedCards.Add(card);
+                }
+            }
+        }
+    }
 }
