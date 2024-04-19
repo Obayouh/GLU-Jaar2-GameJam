9b296c228ffb7033f8a8fc0d92430e992aead3b1
Casper <casper.ketelaars@icloud.com>
2024-04-19 14:16:20 +0200

    IDK test shit

---
+++ GameJam_Itch_io/Assets/Scripts/Draggable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+
+    public enum Slot { Enemy};
+    public Slot typeOfItem = Slot.Enemy;
+
+    GameObject placeHolder = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentToReturnTo = transform.parent;
+        this.transform.SetParent(transform.root);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
