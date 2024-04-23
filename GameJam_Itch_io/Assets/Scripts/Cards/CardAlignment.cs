8f3ba79f337be555819026b454566d1444bd764d
Casper <casper.ketelaars@icloud.com>
2024-04-23 10:48:35 +0200

    SHIIIIITTTT

---
+++ GameJam_Itch_io/Assets/Scripts/Cards/CardAlignment.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace
+{
+    public enum CardAlignment
+    {
+        Left, Center, Right
+    }
+}
---
+++ GameJam_Itch_io/Assets/Scripts/Cards/config/CardPlayConfig.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace config
+{
+    [Serializable]
+    public class CardPlayConfig
+    {
+        [SerializeField]
+        public RectTransform playArea;
+
+        [SerializeField]
+        public bool destroyOnPlay;
+
+    }
+}
