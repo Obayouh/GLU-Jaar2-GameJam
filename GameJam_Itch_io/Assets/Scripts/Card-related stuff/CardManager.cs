0e7a4e4fddffaf528efbc6f11844ec4fcf155f74
Casper <casper.ketelaars@icloud.com>
2024-04-23 14:35:44 +0200

    SHIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIT voor cards niet af

--- GameJam_Itch_io/Assets/Scripts/Card-related stuff/CardManager.cs
+++ GameJam_Itch_io/Assets/Scripts/Card-related stuff/CardManager.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    public GameObject[] cards;
+    public GameObject[] cardPrefabs;
     public List<GameObject> spawnedCards = new List<GameObject>();
     public Transform[] cardPositions;
 
     int maxAmountOfCards = 4;
 
+    public GameObject currentCard;
+    Vector3 startPos;
+
     void Start()
     {
         for (int i = 0; i < maxAmountOfCards; i++)
         {
-            int rdm = Random.Range(0, cards.Length);
-            GameObject card = Instantiate(cards[rdm], cardPositions[i]);
+            int rdm = Random.Range(0, cardPrefabs.Length);
+            GameObject card = Instantiate(cardPrefabs[rdm], cardPositions[i]);
             spawnedCards.Add(card);
         }
+        SelectedCard(spawnedCards[1]);
+
+        Invoke("SwitchCard", 5f);
+    }
+
+    public void SwitchCard()
+    {
+        SelectedCard(spawnedCards[2]);
     }
 
     public void RemoveCard(GameObject card)
     {
         spawnedCards.Remove(card);
+        Destroy(card);
     }
 
     public void AddCards()
@@ -31,13 +43,33 @@ public class CardManager : MonoBehaviour
         {
             for (int i = 0; i < maxAmountOfCards; i++)
             {
-                if (cardPositions[i].childCount >= 0)
+                if (cardPositions[i].childCount <= 0)
                 {
-                    int rdm = Random.Range(0, cards.Length);
-                    GameObject card = Instantiate(cards[rdm], cardPositions[i]);
+                    int rdm = Random.Range(0, cardPrefabs.Length);
+                    GameObject card = Instantiate(cardPrefabs[rdm], cardPositions[i]);
                     spawnedCards.Add(card);
                 }
             }
         }
+    }
+
+    public void SelectedCard(GameObject card)
+    {
+        Debug.Log("test");
+
+        if (currentCard == null)
+        {
+            currentCard = card;
+        }
+
+        if (currentCard != card)
+        {
+            currentCard.transform.position = startPos;
+        }
+
+        currentCard = card;
+
+        startPos = card.transform.position;
+        card.transform.position += new Vector3(card.transform.position.x, card.transform.position.y, 1);
     }
 }
