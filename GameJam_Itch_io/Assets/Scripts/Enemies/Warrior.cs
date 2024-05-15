b896796b6a58dfff2127d17a30523e05ac9097c6
Fabian Dekker <75685544+Flexi4b@users.noreply.github.com>
2024-05-15 12:04:01 +0200

    Warrior Enemy script works
    Going to add the skeleton next

---
+++ GameJam_Itch_io/Assets/Scripts/Enemies/Warrior.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : Ab_Enemy
+{
+    public enum TheStateOfTheWarior
+    {
+        Attack,
+        BuffThemself,
+        Waiting,
+    }
+
+    [SerializeField, Range(1, 50)] private int _MinAttackPower;
+    [SerializeField, Range(2, 100)] private int _MaxAttackPower;
+
+    public TheStateOfTheWarior StateOfTheWarior;
+
+    private Animator WarriorAnim;
+    private PlayerStats _playerStats;
+
+    private float _maxLife;
+    private float _halfLife;
+
+    private bool _attackBuff;
+    private int _detectHalfLife;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        Array arrayElement = Enum.GetValues(typeof(Element));
+        Elements = (Element)arrayElement.GetValue(UnityEngine.Random.Range(0, arrayElement.Length));
+
+        _playerStats = _player.GetComponent<PlayerStats>();
+        _maxLife = GetComponent<HealthSystem>().CurrentHealth;
+        _halfLife = _maxLife / 2;
+    }
+
+    void Update()
+    {
+        if (StateOfTheWarior == TheStateOfTheWarior.Attack)
+        {
+            AttackPlayer();
+        }
+
+        if (StateOfTheWarior == TheStateOfTheWarior.BuffThemself)
+        {
+            BuffItself();
+        }
+
+        //if (StateOfTheWarior == TheStateOfTheWarior.Waiting)
+        //{
+        //    CurrentlyWaiting();
+        //}
+    }
+
+    public override void OnAction()
+    {
+        if (_healthSystem.CurrentHealth <= _halfLife && _detectHalfLife == 0)
+        {
+            _detectHalfLife++;
+            StateOfTheWarior = TheStateOfTheWarior.BuffThemself;
+        }
+        else
+        {
+            StateOfTheWarior = TheStateOfTheWarior.Attack;
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        _playerStats.TakeDamage(UnityEngine.Random.Range(_MinAttackPower, _MaxAttackPower));
+        
+        StateOfTheWarior = TheStateOfTheWarior.Waiting;
+    }
+
+    private void BuffItself()
+    {
+        _MinAttackPower *= 2;
+        _MaxAttackPower *= 2;
+
+        StateOfTheWarior = TheStateOfTheWarior.Waiting;
+    }
+
+    //private void CurrentlyWaiting()
+    //{
+    //    return;
+    //}
+}
