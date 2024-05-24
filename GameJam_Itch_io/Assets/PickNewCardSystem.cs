5897b9161cef7efbc0b8d50d9d28eb8c035a608d
Casper <casper.ketelaars@icloud.com>
2024-05-24 13:25:02 +0200

    PickNewCardSystem niet af

--- GameJam_Itch_io/Assets/PickNewCardSystem.cs
+++ GameJam_Itch_io/Assets/PickNewCardSystem.cs
@@ -8,17 +8,43 @@ public class PickNewCardSystem : MonoBehaviour
     [SerializeField] private Sprite[] cards;
     [SerializeField] private Image[] cardPos;
 
+    [SerializeField] private GameObject chooseNewCardsUI;
+
     private void Start()
     {
-        PickCard();
+
     }
 
     public void PickCard()
     {
+        chooseNewCardsUI.SetActive(true);
+
         for (int i = 0; i < cardPos.Length; i++)
         {
             Sprite card = cards[Random.Range(0, cards.Length)];
             cardPos[i].sprite = card;
         }
+    }
+
+    public void Card1()
+    {
+        StartCoroutine(CardChoosen());
+    }
+
+    public void Card2()
+    {
+        StartCoroutine(CardChoosen());
+    }
+
+    public void Card3()
+    {
+        StartCoroutine(CardChoosen());
+    }
+
+    private IEnumerator CardChoosen()
+    {
+        chooseNewCardsUI.SetActive(false);
+
+        yield return null;
     }
 }
