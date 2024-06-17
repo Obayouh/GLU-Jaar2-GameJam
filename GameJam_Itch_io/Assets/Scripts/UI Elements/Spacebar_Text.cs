6f41865c1ffd707c1dc92320d9c8413caf30f142
Casper <casper.ketelaars@icloud.com>
2024-06-17 13:53:49 +0200

    Canvas Shit en SpaceBar text gaat nu weg wanneer het daadwerkelijk gebruikt wordt

--- GameJam_Itch_io/Assets/Scripts/UI Elements/Spacebar_Text.cs
+++ GameJam_Itch_io/Assets/Scripts/UI Elements/Spacebar_Text.cs
@@ -4,7 +4,7 @@ public class Spacebar_Text : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && ReferenceInstance.refInstance.turnManager.ReturnState() == TurnState.PickNewCard)
         {
             this.gameObject.SetActive(false);
         }
